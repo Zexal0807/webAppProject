82cc0e9ecb2fa3022d2bd6c6f94ff84c8f7ca11c
Zexal0807 <gallinar00@gmail.com>
2024-08-05 10:09:13 +0200

    Add bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter  } from 'react-router-dom';
 
 import App from './App';
 import './global.css'
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
