c0579b6fda1bc1fb6efbbae47951013ba1972cca
Zexal0807 <gallinar00@gmail.com>
2024-08-05 10:15:09 +0200

    Create Hero

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
+import Hero from './components/hero/Hero';
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<>CIAO1</>} />
+      <Route path="/" element={<><Hero/></>} />
       <Route path="/products/:slug" element={<>CIAO2</>} />
     </Routes>
   );
---
+++ client/src/components/hero/Hero.jsx
@@ -0,0 +1,67 @@
+import './Hero.css'
+
+export default function Hero() {
+    return (
+        <div className="hero pt-3 d-flex justify-content-center">
+            <div className="col-8 d-flex flex-column justify-content-center">
+
+                <div className="col-12 d-flex justify-content-between align-items-center">
+                    <img className="col-1 rounded-circle"
+                        src="images/Logo Ospedale Verona.png"
+                        alt="Logo Ospedale di Verona"
+                    />
+                    <h2>
+                        Azienda Ospedialiera Universitaria Integrata di Verona    
+                    </h2>
+                    <img className="col-1 rounded-circle"
+                        src="images/Logo Università Verona.png"
+                        alt="Logo Università di Verona"
+                    />
+                </div>
+
+                <div className="col-12 d-flex justify-content-center align-items-center">
+                    <div className="col-4 d-grid align-items-center">
+                        <img className="col-11 rounded-circle"
+                            src="images/Logo MISTRA.jpg"
+                            alt="Logo MISTRA"
+                        />
+                        <div className="text-center text-primary">
+                            <span style={{color:"#3CBA25"}}>SE</span>
+                            <span style={{color:"#F7D631"}}>XU</span>
+                            <span style={{color:"#F77A0C"}}>AL </span>
+                            <br/>
+                            <span style={{color:"#E80C0C"}}>HE</span>
+                            <span style={{color:"#F0B6C6"}}>AL</span>
+                            <span style={{color:"#11BCF0"}}>TH</span>
+                        </div>
+                    </div>
+                    <div className="col-8 d-flex flex-column">
+                        <h1>
+                            CENTRO MISTRA
+                        </h1>
+                        <h2>
+                            <b>Centro</b>
+                        </h2>
+                        <h2>
+                            <b>M</b>ultidisciplinare
+                        </h2>
+                        <h2>
+                            <b>I</b>nfezioni <b>S</b>essualmente <b>Tra</b>smesse
+                        </h2>
+                    </div>
+                </div>
+
+                <div className="col-12 p-3"></div>
+
+                <div className="col-12">
+
+                    <a>
+                        HOME
+                    </a>
+
+                </div>
+            </div>
+            
+        </div>
+    );
+}
