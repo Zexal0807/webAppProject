17f00a9f0811d52a838c46287eeb2276e877feeb
Zexal0807 <gallinar00@gmail.com>
2024-11-19 22:07:59 +0100

    Fix frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,12 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Page from "./Page";
-
-const BACKEND_HOST = "http://localhost:3001/";
 
 export default function App() {
 	return (
 		<Routes>
 			<Route path="/app" element={<>TEST APP</>} />
-			<Route path="/:pageslug?" element={<Page host={BACKEND_HOST} />} />
+			<Route path="/:pageId" element={<Page />} />
+			<Route path="/" element={<Navigate to="/home" replace />} />
 		</Routes>
 	);
 }
--- client/src/Page.jsx
+++ client/src/Page.jsx
@@ -1,28 +1,37 @@
-import Hero from "./components/hero/Hero";
-import Navbar from "./components/navbar/Navbar";
+import { redirect, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-export default function Page({ host }) {
-	const [page, setPage] = useState({
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/Navbar";
+
+export default function Page() {
+	const { pageId } = useParams();
+
+	const [content, setContent] = useState({
 		title: "",
 	});
 
 	useEffect(() => {
 		const fetchData = async () => {
-			let pageslug = window.location.pathname;
-
-			// const res = await fetch(`${host}/page/${pageslug}`)
-			//     .then(data => data.json())
-			//     .catch(e => {
-			//         window.location.href = "/"
-			//     })
-
-			setPage({
-				title: "Home",
-				content: "CIAO",
-			});
-			console.log(page);
-			document.title = page.title;
+			try{
+				const response = await fetch(`${process.env.BACKEND_HOST}/api/pages/${pageId}`, {
+					headers: {
+						Authorization: `Bearer 2c9ff8461fe9ba60bd4ffe56e2493db713a7cac418f918d7f568c93ee48b9314365cee0fe07dec1402b8955b8cb05f0d88c60ff52462b1694e1884ceeaf6b5e40f56f54808afcfe7ff07ad71370cf9b1e3a068ff22c79257b2bf548e87acf5899a0f495b30bf62db8b70ef1b1ab1afbab93c581105596cd9af59233ee7583f16`,
+					}
+				});
+				if (response.ok) {
+					const data = await response.json();
+					setContent(data);
+				}
+			} catch(e) {
+				// redirect("/");
+				setContent({
+					title: "Home",
+					content: "CIAO",
+				});
+			};
+			console.log(content);
+			document.title = content.title;
 		};
 
 		fetchData();
@@ -32,7 +41,9 @@ export default function Page({ host }) {
 		<>
 			<Hero />
 			<Navbar />
-			<div></div>
+			<div>{content.title}</div>
+
+			{/* Per ogni elemento dell'array del content, creo il component corrispondente */}
 		</>
 	);
 }
