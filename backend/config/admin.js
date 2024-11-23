a3e6982d980a2177394778abf61a3639c50249bf
Zexal0807 <gallinar00@gmail.com>
2024-11-23 14:41:41 +0100

    Merge env

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'nFlvGQ8RaNb0YbNuZVQo8g=='),
+    secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT', 'YPzfoSM2ApCOiyjOZu1fkA=='),
+    salt: env('API_TOKEN_SALT'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT', 'keHfOSyvGNGfq2PxD+F3Fw=='),
+      salt: env('TRANSFER_TOKEN_SALT'),
     },
   },
   flags: {
--- backend/config/server.js
+++ backend/config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
-  host: env('STRAPI_HOST', '0.0.0.0'),
-  port: env.int('STRAPI_PORT', 1337),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS', ['mySecretKeyA', 'mySecretKeyB']), // valore di fallback se non ci sono variabili
+    keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
