d5dea079e18768543e11f50f055b41d6596b9623
Zexal0807 <gallinar00@gmail.com>
2024-11-28 17:55:44 +0100

    Remove not used screen

--- frontend/src/components/team/Team.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Team({ value }) {
-    return (
-        <div className="team-component">
-            <h1>{value}</h1>
-        </div>
-    );
-}
--- frontend/src/components/textTeam/TextTeam.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function TextTeam({ value }) {
-    return (
-        <p className="textteam-component">{value}</p>
-    );
-}
