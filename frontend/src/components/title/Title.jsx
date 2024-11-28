b456d449a95db4ae33810eac89a361fbc89d1dd3
Zexal0807 <gallinar00@gmail.com>
2024-11-28 21:45:18 +0100

    Add title

--- frontend/src/components/title/Title.jsx
+++ frontend/src/components/title/Title.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import './Title.css'
 
 export default function TitleTeam({ value }) {
     return (
-        <div className="titleteam-component">
-            <h4>{value}</h4>
+        <div className="title-component p-3 p-sm-4 mb-2">
+            {value}
         </div>
     );
 }
