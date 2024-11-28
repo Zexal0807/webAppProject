87e986bbe37d00853d1ade22cf1d0dd0689ebbc0
Zexal0807 <gallinar00@gmail.com>
2024-11-28 21:45:25 +0100

    Add text

--- frontend/src/components/text/Text.jsx
+++ frontend/src/components/text/Text.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import DOMPurify from "dompurify";
+
+import './Text.css'
 
 export default function Text({ value }) {
     return (
-        <p className="text-component">{value}</p>
+        <p 
+            className="text-component p-2 p-sm-4" 
+            dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(value) }}
+        ></p>
     );
 }
