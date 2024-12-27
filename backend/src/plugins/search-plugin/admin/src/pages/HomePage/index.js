6fec2dfc2731dd410e24af446750e007baa85824
Zexal0807 <gallinar00@gmail.com>
2024-12-27 15:47:25 +0100

    Implement detail

--- backend/src/plugins/search-plugin/admin/src/pages/HomePage/index.js
+++ backend/src/plugins/search-plugin/admin/src/pages/HomePage/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { TextInput, Button, Box, Typography } from '@strapi/design-system';
+import { Textarea, TextInput, DatePicker, Button, Box, Typography } from '@strapi/design-system';
 import { Search } from '@strapi/icons';
 import { useIntl } from "react-intl";
 import axios from 'axios';
@@ -14,12 +14,18 @@ const Homepage = () => {
   const handleSearch = async () => {
     try {
       const response = await axios.get(`/search-plugin/search?code=${code}`);
-      setResults(response.data.data);
+      setResults([...response.data.data, ...response.data.data]);
       setSelectedEntry(null);
     } catch (error) {
       console.error('Error fetching search results:', error);
     }
   };
+
+  const handleSave = async (e) => {
+    debugger;
+    
+
+  }
 
   return (
     <Box padding={8} background="neutral100">
@@ -34,7 +40,7 @@ const Homepage = () => {
               onChange={(e) => setCode(e.target.value)}
             />
         </div>
-        <div style={{width:"15%",display: "flex", alignItems: "flex-end", justifyContent: "center"}}>
+        <div style={{width:"15%", display: "flex", alignItems: "flex-end", justifyContent: "center"}}>
           <Button onClick={handleSearch} size="L">
             <Search /> {formatMessage({id:'search-plugin.button.search'})}
           </Button>
@@ -43,34 +49,146 @@ const Homepage = () => {
 
       {results.length > 0 && (
         <div style={{paddingTop:"1rem"}}>
-          <Typography variant="beta">Results:</Typography>
+            <Typography variant="beta">{formatMessage({id:'search-plugin.results.title'})}:</Typography>
             <div style={{display:"flex", flexWrap:"wrap", paddingTop:"0.5rem"}}>
                 {results.map((result) => (
                     <div 
                       style={{width: "16%", paddingLeft:"0.5rem", paddingRight:"0.5rem"}}
                       key={result.id}
                     >
-                <Button
+                      <Button
                         style={{width: "100%", justifyContent:"center"}}
-                  variant="secondary"
+                        variant="secondary"
                         size="L"
                         onClick={() => setSelectedEntry(result)}
-                >
+                      >
                         {result.code}
-                </Button>
+                      </Button>
                     </div>
-            ))}
+                ))}
             </div>
         </div>
       )}
 
       {selectedEntry && (
-        <Box paddingTop={6}>
-          <Typography variant="beta">Details:</Typography>
-          <Typography>ID: {selectedEntry.id}</Typography>
-          <Typography>Name: {selectedEntry.name}</Typography>
-          <Typography>Description: {selectedEntry.description}</Typography>
-        </Box>
+        <div style={{paddingTop:"1rem"}}>
+          <Typography variant="beta">{formatMessage({id:'search-plugin.details.title'})}:</Typography>
+          <br />
+
+          <div style={{width:"100%", display: "flex", paddingTop:"0.5rem"}}>
+            <div style={{width:"33%", paddingRight:"1rem"}}>
+              <TextInput
+                label={formatMessage({id:'search-plugin.label.code'})}
+                disabled={true}
+                value={selectedEntry.code}
+              />
+            </div>
+            <div style={{width:"67%", paddingLeft:"1rem"}}>
+              <TextInput
+                label={formatMessage({id:'search-plugin.label.testname'})}
+                disabled={true}
+                value={selectedEntry.test.name}
+              />
+            </div>
+          </div>
+
+          <div style={{width:"100%", paddingTop:"0.5rem"}}>
+            <Textarea
+              label={formatMessage({id:'search-plugin.label.testdescription'})}
+              disabled={true}
+              value={selectedEntry.test.description}
+            />
+          </div>
+
+          <div style={{width:"100%", display: "flex", paddingTop:"0.5rem"}}>
+            <div style={{width:"16.8%", paddingRight:"1rem"}}>
+              <TextInput
+                label={formatMessage({id:'search-plugin.label.sex'})}
+                disabled={true}
+                value={selectedEntry.sex.name}
+              />
+            </div>
+            <div style={{width:"16.8%", paddingLeft:"1rem", paddingRight:"1rem"}}>
+              <TextInput
+                label={formatMessage({id:'search-plugin.label.age'})}
+                disabled={true}
+                value={selectedEntry.age}
+              />
+            </div>
+            <div style={{width:"33.3%", paddingRight:"1rem", paddingLeft:"1rem"}}>
+              <TextInput
+                label={formatMessage({id:'search-plugin.label.ip'})}
+                disabled={true}
+                value={selectedEntry.IP}
+              />
+            </div>
+            <div style={{width:"33.3%", paddingLeft:"1rem"}}>
+              <TextInput
+                label={formatMessage({id:'search-plugin.label.score'})}
+                disabled={true}
+                value={selectedEntry.score}
+              />
+            </div>
+          </div>
+
+          <div style={{ 
+            color: "rgb(50, 50, 77)", 
+            fontSize: "0.75rem", 
+            fontWeight: "600", 
+            paddingTop:"0.5rem", 
+            paddingBottom: "0.5rem"
+            }}>
+            {formatMessage({id:'search-plugin.label.questions'})}:
+          </div>
+
+          <div style={{paddingLeft: "1rem", paddingRight:"1rem"}}>
+            {selectedEntry.answers.map(answer => (
+              <div style={{width:"100%", display: "flex", paddingTop:"0.5rem"}}>
+                <div style={{width:"91.6%", paddingRight:"1rem"}}>
+                  <TextInput
+                    label={answer.question.text}
+                    disabled={true}
+                    value={answer.text}
+                  />
+                  {answer.score == 0 && (
+                    <span style={{ color: "rgb(208, 43, 32)", fontSize: "0.75rem", fontWeight: "600" }}>
+                      {answer.correction}
+                    </span>
+                  )}
+                </div>
+                <div style={{width:"8.4%", paddingLeft:"1rem", paddingRight:"1rem"}}>
+                  <TextInput
+                label={formatMessage({id:'search-plugin.label.score'})}
+                    disabled={true}
+                    value={answer.score}
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div style={{width:"100%", display: "flex", paddingTop:"1rem"}}>
+            <div style={{width:"33%", paddingRight:"1rem"}}>
+              <DatePicker
+                label={formatMessage({id:'search-plugin.label.revision_date'})}
+                value={selectedEntry.revision_date}
+              />
+            </div>
+            <div style={{width:"67%", paddingLeft:"1rem"}}>
+              <Textarea
+                label={formatMessage({id:'search-plugin.label.note'})}
+                value={selectedEntry.note}
+              />
+            </div>
+          </div>
+
+          <div style={{width:"100%", display: "flex", paddingTop:"1rem"}}>
+            <Button onClick={handleSave} size="L">
+              <Search /> {formatMessage({id:'search-plugin.button.save'})}
+            </Button>
+          </div>
+
+        </div>
       )}
     </Box>
   );
