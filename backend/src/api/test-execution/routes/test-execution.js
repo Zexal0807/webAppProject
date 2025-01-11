57ee51b8374beb2ec0000d57c76dab139d02c9fe
Zexal0807 <gallinar00@gmail.com>
2025-01-11 13:10:08 +0100

    Fix

--- backend/src/api/test-execution/routes/test-execution.js
+++ backend/src/api/test-execution/routes/test-execution.js
@@ -6,15 +6,6 @@ module.exports = {
             handler: 'test-execution.updateInstance',
             config: {
                 auth: false
-'use strict';
-
-/**
- * test-execution router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::test-execution.test-execution');
             },
         },
     ],
