6fee0b9a113bdf88c86359450dd6ce93995086d6
Zexal0807 <gallinar00@gmail.com>
2025-01-11 14:04:00 +0100

    Remove not used api

--- backend/src/api/test-execution/services/test-execution.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * test-execution service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::test-execution.test-execution');
